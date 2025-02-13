0417fde83dd900e552cdd745fcd5890b8b3b26e5
tanishbajaj101 <tanishbajaj101@gmail.com>
2025-02-13 11:08:07 +0530

    sqlite to postgresql migration

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///tags.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
     
@@ -9,4 +9,6 @@ class Config:
     YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
     GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
     GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
+    BASE_URI = os.getenv('BASE_URL')
     GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
+    
